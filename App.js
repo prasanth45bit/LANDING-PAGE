2f5499baf9c4bb64a9530799bda9eed84798476a
prasanth45bit <155311223+prasanth45bit@users.noreply.github.com>
2024-04-19 14:29:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Land from './Landing';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Land />
+      </div>
+  );
+}
+
+export default App;
